dacaa26c24279ff04373a64ca84f6b3c202bd21a
Duygu Öğünç <Section2@WISSEN>
2019-04-25 20:10:55 +0300

    Proje dosyası ekle.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult GetStudents()
+        {
+            var students = new List<Student>();
+            students.Add(new Student { FirstName = "Neslihan", LastName = "Kalyoncu", Phone = "5550000", Salary = 2200 });
+            students.Add(new Student { FirstName = "Engin", LastName = "Karabudak", Phone = "5550000", Salary = 2100 });
+            return Json(students);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication3/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
